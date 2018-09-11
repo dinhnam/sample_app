f8d9219f9003fb55c61001c67e0fb7d465c4dd61
Dinh Van Nam <dinhvannambkhn@gmail.com>
2018-09-11 09:15:43 +0700

    chapter 9: advance login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,48 @@
 class User < ApplicationRecord
-  before_save{self.email = email.downcase}
-  validates :name,  presence: true, length: {maximum:
-    Settings.validates.max_name}
+  attr_accessor :remember_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum:
-    Settings.validates.max_email},
-      format: {with: VALID_EMAIL_REGEX}, uniqueness: true
-  has_secure_password
-  validates :password, presence: true, length: {minimum:
-    Settings.validates.min_password}
+
+  def self.save
+    before_save{self.email = email.downcase}
+    validates :name,  presence: true, length: {maximum:
+      Settings.validates.max_name}
+
+    validates :email, presence: true, length: {maximum:
+      Settings.validates.max_email},
+        format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+    has_secure_password
+    validates :password, presence: true, length: {minimum:
+        Settings.validates.min_password}
+  end
+
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create string, cost: cost
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update remember_digest: nil
+  end
+
+  begin
+    save
+  end
 end
