961ab41447ce072a8699a21a1d2f54ddd93c304b
Dinh Van Nam <dinhvannambkhn@gmail.com>
2018-09-11 09:15:43 +0700

    chap 10 part 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) # Not the final implementation!
     if @user.save
-      # Handle a successful save.
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render "new"
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
