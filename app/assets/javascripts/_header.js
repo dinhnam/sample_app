4e7ab323d5157b0939f2ea398b54a9a2abbc1866
Dinh Van Nam <dinhvannambkhn@gmail.com>
2018-09-15 00:59:15 +0700

    chapter 13: user micropost

--- app/assets/javascripts/_header.js
+++ app/assets/javascripts/_header.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-  $('.dropdown').click(function(){
-    $('.dropdown-menu').toggle();
+  $(".dropdown").click(function(){
+    $(".dropdown-menu").toggle();
+  });
+  $("#micropost_picture").bind("change", function() {
+    var size_in_megabytes = this.files[0].size/1024/1024;
+    if (size_in_megabytes > 5) {
+      alert("Maximum file size is 5MB. Please choose a smaller file.");
+    }
   });
 });
+
